9fe7ab04bfe023447c051bb5dcd5ceceb23b7bc8
topli <199720923@qq.com>
2019-05-23 14:00:35 +0800

    bug

--- src/lang/zh/app.js
+++ src/lang/zh/app.js
@@ -9,6 +9,7 @@ export default {
     fileTypeNullError: '未设置文件类型，请联系管理员。',
     delTips: '此操作将永久删除该数据, 是否继续?',
     delete: '删除',
+    disables: '禁用',
     submit: '确定',
     cancel: '取消',
     success: '成功',
